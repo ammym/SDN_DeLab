c1d771cb948f036dcca437af562ac80fcc7a6bc9
ammym <amyyuliana@gmail.com>
2022-07-24 10:41:36 -0400

    Segundo Commit
    Se agrega modificación en script.py, este devuelve la lista de las organizaciones accedidas con la API-Key, además, crea archivo Inventario.csv con los dispositivos wireless y appliance de la compañia DeLab que se puede acceder con la API-Key asignada
    
    Se agrega archivo .gitignore para no guardar en el repositorio el archivo Inventario.csv generado por script.py

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
 import requests
 from pprint import pprint
+import json
+import csv
 
-#--------------------URL------------------------------
+
+#--------------------URL's------------------------------
 url="https://api.meraki.com/api/v1/organizations/"
 
 #----------------API-Key---------------------------------
@@ -21,7 +24,73 @@ def getOrganizations (): #Imprime y devuelve la lista de las organizaciones
 	
 	return res_org
 
-org = getOrganizations()
+def getOrganizationsDevices (org): #Devuelve la lista de dispositivos de DeLab en archivo "Inventario.csv"
+	for i in range(len(org)):
+		if org[i]["name"] == "DeLab":
+			id=org[i]["id"]
+			DevicesList(id)
+			break
+
+def DevicesList(id):
+	
+	id_devices = id + "/devices"
+
+	url_combine = url + id_devices
+
+	response = requests.get(url_combine, headers=Key)
+
+	res_devices = response.json()  #Lista de dispositivos
+
+	res_status = DevicesStatuses(id) #Lista de status de dispositivos
+	
+	devices = []
+	Dict = {}
+
+	for i in range (len(res_devices)):
+		if res_devices[i]["productType"] == "wireless" or res_devices[i]["productType"] == "appliance":
+	
+			Dict["Tipo_Producto"] = res_devices[i]["productType"]
+			Dict["Modelo"] = res_devices[i]["model"]
+			Dict["Nombre"] = res_devices[i]["name"]
+			Dict["Direccion_MAC"] = res_devices[i]["mac"]
+			Dict["Serial"] = res_devices[i]["serial"]
+			if res_devices[i]["productType"] == "wireless":
+				Dict["Direccion_IP_LAN"] = res_devices[i]["lanIp"]
+			else:	
+				Dict["Direccion_IP_LAN"] = ""
+			for l in range(len(res_status)):
+				if res_status[l]["serial"] == res_devices[i]["serial"] and res_status[l]["name"]==res_devices[i]["name"]:
+					Dict["Direccion_IP_Publica"] = res_status[l]["publicIp"]
+					Dict["Status"] = res_status[l]["status"]
+			devices.append(Dict)
+			Dict = {}
+
+	DicttoCSV(devices)
+
+def DevicesStatuses (id):
+
+	id_devices_status = id + "/devices/statuses"
+
+	url_combine = url + id_devices_status
+
+	response = requests.get(url_combine, headers=Key)
+	
+	res_status = response.json()
+		
+	return res_status
+
+def DicttoCSV (devices):
+
+	keys = devices[0].keys()
+
+	with open('Inventario.csv',mode='w') as inventario:
+		name_w=csv.DictWriter(inventario,delimiter=',', fieldnames = keys)
+		name_w.writeheader()
+		name_w.writerows(devices)
+
+org=getOrganizations()
+getOrganizationsDevices (org)
+
 
 
 
